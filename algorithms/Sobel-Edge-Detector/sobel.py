9dbaf154fd19f051eff7ac4aab6f0c09ad0c9e6b
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2022-04-25 00:57:18 +0430

    fix sobel algorithm problems and add thresholding

--- algorithms/Sobel-Edge-Detector/sobel.py
+++ algorithms/Sobel-Edge-Detector/sobel.py
@@ -4,39 +4,58 @@ import cv2
 import numpy as np
 from scipy.signal import convolve2d
 
-def convolve_and_scale_to_255(image, kernel):
-    convolved_matrix = np.abs(convolve2d(image, kernel, mode='same', boundary='symm'))
-    return np.array((convolved_matrix / np.max(convolved_matrix)) * 255, dtype=np.float64)
+def convolve(image, kernel):
+    convolved_matrix = convolve2d(image, kernel, mode='same', boundary='symm')
+    return np.array(convolved_matrix, dtype=np.float64)
 
-#Loading the image
-root = tk.Tk()
-root.withdraw()
-img_path = fd.askopenfilename()
-root.destroy()
-img_name = img_path.split('/')[-1]
-img = cv2.imread(img_path, 0)
+def sobel_algorithm(img):
+    # Apply Sobel Gx kernel
+    x_gradient_matrix = convolve(img, gx_kernel)
+    # Apply Sobel Gy kernel
+    y_gradient_matrix = convolve(img, gy_kernel)
+    # Combine the gradient matrices to get the gradient magnitude
+    return np.sqrt(x_gradient_matrix**2 + y_gradient_matrix**2)
 
-# Apply Sobel Gx kernel
+def on_tr_trackbar(val):
+    global gradient_image, threshold_tracker, img
+    gradient_magnitude = sobel_algorithm(img)
+
+    threshold_tracker = int(cv2.getTrackbarPos('Threshold', 'Trackbars') * np.max(gradient_magnitude) / 500)
+    
+    gradient_image = np.zeros(gradient_magnitude.shape, dtype=np.uint8)
+    gradient_image[gradient_magnitude > threshold_tracker] = 255
+    gradient_image = cv2.putText(gradient_image, f'T: {threshold_tracker}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+    gradient_image = cv2.putText(gradient_image, f'T: {threshold_tracker}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+    # Show results
+    cv2.imshow('Gray Scale', img)
+    cv2.imshow('Sobel Operator', gradient_image)
+
+IMAGE_FILE = '../../test-images/mosalahNoisy.png'
+IMAGE_NAME = IMAGE_FILE.split('/')[-1]
+img = cv2.imread(IMAGE_FILE, 0)
+
+threshold_tracker = 0
+
+gradient_image = None
+
 gx_kernel = np.array([[1, 0, -1],
                       [2, 0, -2],
                       [1, 0, -1]])
-x_gradient_matrix = convolve_and_scale_to_255(img, gx_kernel)
 
-# Apply Sobel Gy kernel
 gy_kernel = np.array([[1, 2, 1],
                       [0, 0, 0],
                       [-1, -2, -1]])
-y_gradient_matrix = convolve_and_scale_to_255(img, gy_kernel)
 
-# Combine the two gradient matrices
-gradient_image = np.sqrt(x_gradient_matrix**2 + y_gradient_matrix**2)
-gradient_image = np.array((gradient_image / np.max(gradient_image)) * 255, dtype=np.uint8)
+#Creating trackbar window
+cv2.namedWindow('Trackbars')
+cv2.resizeWindow('Trackbars', 640, 240)
+cv2.createTrackbar("Threshold", "Trackbars", 0, 500, on_tr_trackbar)
 
-# Show results
-cv2.imshow('Gray Scale', img)
-cv2.imshow('Sobel Operator', gradient_image)
+on_tr_trackbar(50)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# Save image
-cv2.imwrite('results/' + img_name, gradient_image)
+gradient_image = cv2.putText(gradient_image, f'T: {threshold_tracker}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+cv2.imwrite('results/' + IMAGE_NAME, gradient_image)
