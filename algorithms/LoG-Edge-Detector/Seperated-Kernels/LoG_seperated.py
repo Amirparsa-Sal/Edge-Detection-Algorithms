800ced28030a507187cb6fd187858723a8221fe0
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2022-06-07 18:30:46 +0430

    improve hysteresis tracking in LoG

--- algorithms/LoG-Edge-Detector/Seperated-Kernels/LoG_seperated.py
+++ algorithms/LoG-Edge-Detector/Seperated-Kernels/LoG_seperated.py
@@ -1,5 +1,3 @@
-# import tkinter as tk
-# import tkinter.filedialog as fd
 from typing import final
 import cv2
 import numpy as np
@@ -55,7 +53,7 @@ def zero_crossing(image):
                 elif image[i,j]<0:
                     z_c_image[i, j] = np.abs(image[i,j]) + d
  
-    return z_c_image
+    return np.uint8(z_c_image / np.max(z_c_image) * 255)
 
 def double_threshold_image(img, low_thresh, high_thresh, weak_value=70, strong_value=255):
     # Thresholding the image
@@ -69,86 +67,74 @@ def double_threshold_image(img, low_thresh, high_thresh, weak_value=70, strong_v
     new_image[zeros] = 0
     return new_image
 
+def is_valid_pixel(img, i, j):
+    return i < img.shape[0] and i >= 0 and j < img.shape[1] and j >= 0
+
+def valid_neighbors(img, i, j):
+    neighbors = []
+    for k in range(-1, 2 , 1):
+        for p in range(-1, 2, 1):
+            if is_valid_pixel(img, i + k, j + p):
+                neighbors.append((i + k, j + p))
+    return neighbors
+    
 def hysteresis_tracking(img, weak_value=70, strong_value=255):
     img_row, img_col = img.shape
- 
-    top_to_bottom = img.copy()
-    for row in range(1, img_row):
-        for col in range(1, img_col):
-            if top_to_bottom[row, col] == weak_value:
-                if any_neighbor_has_value(top_to_bottom, row, col, strong_value):
-                    top_to_bottom[row, col] = strong_value
-                else:
-                    top_to_bottom[row, col] = 0
- 
-    bottom_to_top = img.copy()
-    for row in range(img_row - 1, 0, -1):
-        for col in range(img_col - 1, 0, -1):
-            if bottom_to_top[row, col] == weak_value:
-                if any_neighbor_has_value(bottom_to_top, row, col, strong_value):
-                    bottom_to_top[row, col] = strong_value
-                else:
-                    bottom_to_top[row, col] = 0
- 
-    right_to_left = img.copy()
-    for row in range(1, img_row):
-        for col in range(img_col - 1, 0, -1):
-            if right_to_left[row, col] == weak_value:
-                if any_neighbor_has_value(right_to_left, row, col, strong_value):
-                    bottom_to_top[row, col] = strong_value
-                else:
-                    right_to_left[row, col] = 0
- 
-    left_to_right = img.copy()
-    for row in range(img_row - 1, 0, -1):
-        for col in range(1, img_col):
-            if left_to_right[row, col] == weak_value:
-                if any_neighbor_has_value(left_to_right, row, col, strong_value):
-                    bottom_to_top[row, col] = strong_value
-                else:
-                    left_to_right[row, col] = 0
- 
-    final_img = top_to_bottom + bottom_to_top + right_to_left + left_to_right
- 
-    final_img[final_img > 255] = 255
- 
-    return final_img
+    
+    new_img = img.copy()
+    for i in range(img_row):
+        for j in range(img_col):
+            if new_img[i,j] == strong_value:
+                queue = []
+                for neighbor in valid_neighbors(new_img, i, j):
+                    if new_img[neighbor[0], neighbor[1]] == weak_value:
+                        queue.append(neighbor)
+                while len(queue) > 0:
+                    x, y = queue.pop(0)
+                    if new_img[x,y] == weak_value:
+                        new_img[x,y] = strong_value
+                        for neighbor in valid_neighbors(new_img, x, y):
+                            if new_img[neighbor[0], neighbor[1]] == weak_value:
+                                queue.append(neighbor)
 
-def LoG_seperated(img, kernel_size, sigma, low_threshold, high_threshold):
-    global log_image, final_image, kernel_tracker, sigma_tracker
+    for i in range(img_row):
+        for j in range(img_col):
+            if new_img[i,j] == weak_value:
+                new_img[i,j] = 0
+
+    return new_img
+
+def LoG_seperated(img):
+    global log_image, final_image, kernel_tracker, sigma_tracker, low_threshold_tracker, high_threshold_tracker
     #Smoothing the image using gaussian kernel
-    if sigma != 0:
-        blurred_image = cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma)
+    if sigma_tracker != 0:
+        blurred_image = cv2.GaussianBlur(img, (kernel_tracker, kernel_tracker), sigma_tracker)
     else:
         blurred_image = img
+    cv2.imshow('Gaussian Blur', blurred_image)
     # Apply laplacian kernel
     kernel = np.array([[1, 1, 1],
                         [1, -8, 1],
                         [1, 1, 1]])
     
     log_image = zero_crossing(convolve(blurred_image, kernel))
-    print(np.max(log_image))
-    sigma_tracker = sigma
-    kernel_tracker = kernel_size
+    cv2.imshow('zero crossing', log_image)
+    l_image = zero_crossing(convolve(img, kernel))
+    cv2.imshow('zero crossing original', l_image)
     # Thresholding the image
-    low_threshold = int(low_threshold * np.max(log_image) / 500)
-    high_threshold = int(high_threshold * np.max(log_image) / 500)
-    thresh_image = double_threshold_image(log_image, low_threshold, high_threshold)
+    thresh_image = double_threshold_image(log_image, low_threshold_tracker, high_threshold_tracker)
+    cv2.imshow('Thresholded image', thresh_image)
     final_image = hysteresis_tracking(thresh_image)
-    final_image = cv2.putText(final_image, f'LT: {low_threshold} HT: {high_threshold} K: {kernel_tracker}, Sig: {sigma_tracker}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+    final_image = cv2.putText(final_image, f'LT: {low_threshold_tracker} HT: {high_threshold_tracker} K: {kernel_tracker}, Sig: {sigma_tracker}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
 
     # Show results
-    cv2.imshow('Gaussian Blur', blurred_image)
-    cv2.imshow('Thresholded image', thresh_image)
     cv2.imshow('Final image', final_image)
 
 def handle_trackbar():
-    global img, threshold_tracker
-    sigma = cv2.getTrackbarPos('Sigma', 'Trackbars')
-    kernel_size = cv2.getTrackbarPos('K', 'Trackbars')
-    kernel_size = (kernel_size+1) * 2 + 1
-    threshold_tracker = cv2.getTrackbarPos('K', 'Trackbars')
-    LoG_seperated(img, kernel_size, sigma, low_threshold_tracker, high_threshold_tracker)
+    global img, threshold_tracker, kernel_tracker, sigma_tracker
+    sigma_tracker = int(cv2.getTrackbarPos('Sigma', 'Trackbars'))
+    kernel_tracker = int(cv2.getTrackbarPos('K', 'Trackbars') + 1) * 2 + 1
+    LoG_seperated(img)
 
 def on_sigma_trackbar(val):
     handle_trackbar()
@@ -158,14 +144,12 @@ def on_kernel_trackbar(val):
 
 def handle_threshold_trackbars():
     global log_image, threshold_tracker, final_image
-    low_threshold_tracker = cv2.getTrackbarPos('Low Threshold', 'Trackbars')
-    high_threshold_tracker = cv2.getTrackbarPos('High Threshold', 'Trackbars')
-    low_threshold = int(low_threshold_tracker * np.max(log_image) / 500)
-    high_threshold = int(high_threshold_tracker * np.max(log_image) / 500)
+    low_threshold_tracker = int(cv2.getTrackbarPos('Low Threshold', 'Trackbars'))
+    high_threshold_tracker =int(cv2.getTrackbarPos('High Threshold', 'Trackbars'))
 
-    thresh_image = double_threshold_image(log_image, low_threshold, high_threshold)
+    thresh_image = double_threshold_image(log_image, low_threshold_tracker, high_threshold_tracker)
     final_image = hysteresis_tracking(thresh_image)
-    final_image = cv2.putText(final_image, f'LT: {low_threshold} HT: {high_threshold} K: {kernel_tracker}, Sig: {sigma_tracker}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    final_image = cv2.putText(final_image, f'LT: {low_threshold_tracker} HT: {high_threshold_tracker} K: {kernel_tracker}, Sig: {sigma_tracker}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
     cv2.imshow('Thresholded image', thresh_image)
     cv2.imshow('Final image', final_image)
 
@@ -177,7 +161,8 @@ def on_high_thresh_trackbar(val):
 
 
 
-IMAGE_FILE = '../../../test-images/lena.png'
+IMAGE_FILE = '../../../test-images/lena-noisy.jpeg'
+IMAGE_NAME = IMAGE_FILE.split('/')[-1]
 img = cv2.imread(IMAGE_FILE, 0)
 log_image = img
 final_image = img
@@ -191,13 +176,16 @@ cv2.namedWindow('Trackbars')
 cv2.resizeWindow('Trackbars', 640, 240)
 cv2.createTrackbar("K", "Trackbars", 1, 20, on_kernel_trackbar)
 cv2.createTrackbar("Sigma", "Trackbars", 1, 24, on_sigma_trackbar)
-cv2.createTrackbar("Low Threshold", "Trackbars", 1, 500, on_low_thresh_trackbar)
-cv2.createTrackbar("High Threshold", "Trackbars", 1, 500, on_high_thresh_trackbar)
+cv2.createTrackbar("Low Threshold", "Trackbars", 1, 255, on_low_thresh_trackbar)
+cv2.createTrackbar("High Threshold", "Trackbars", 1, 255, on_high_thresh_trackbar)
 
-# Save image
-# cv2.imwrite('results/' + img_name, log_image)
+
 on_sigma_trackbar(1)
 on_kernel_trackbar(1)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Save image
+final_image = cv2.putText(final_image, f'LT: {low_threshold_tracker} HT: {high_threshold_tracker} K: {kernel_tracker}, Sig: {sigma_tracker}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+cv2.imwrite('results/' + IMAGE_NAME, final_image)
